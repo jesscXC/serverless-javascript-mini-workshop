027fd39a40110887204619559a769512b33d57d9
Jesse Cummins <me@jessecummins.com>
2019-10-30 20:38:29 -0400

    Update upcaseFn to use `greetings` param

--- upcaseFn.js
+++ upcaseFn.js
@@ -1,5 +1,4 @@
 function main(params) {
-  let name = params.name || 'User';
-  let place = params.place || 'Earth';
-  return { greetings: 'Hello, ' + name + ' from location: ' + place };
+  let greetings = params.greetings;
+  return { greeting: greetings };
 }
